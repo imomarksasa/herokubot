6e9953eded5d971c88ba36cdc4cfe12286298a19
Didixo <didixo0@hotmail.com>
2017-06-10 00:14:09 +0200

    removed express

--- bot.js
+++ bot.js
@@ -1,21 +1,9 @@
-var express = require("express");
-var app = express();
 var fs = require("fs");
 var Discordie = require("discordie");
 var client = new Discordie();
 var prefix = "!"
 var donotdisturb = false;
 var trapCount = 0;
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
 
 fs.stat("trapamount.txt", (err, stat) => {
 	if(err == null) {
