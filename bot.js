08fad8f3b9270533c03956551ee3861b66a1a579
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-18 23:51:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,30 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-
-
+const fs = require('fs');
+const moment = require('moment');
+const jimp = require('jimp');
+const Canvas = require('canvas');
+ const prefix = "-";
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by Preficter');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
 });
 
 
@@ -12,59 +32,39 @@ client.on('ready', () => {
 
 
 
-
-if (!fs) {
-    var fs = require(`fs`);
+const devs = ["321476121956974595"];
+const adminprefix = ["-"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith('ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === ("leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith('wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('mils')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith('st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith('setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith('setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
-var data = JSON.parse(fs.readFileSync(`./data.json`, `UTF8`));
-if (!prefix) {
-    var prefix = data.prefix;
-} else {
-    prefix = data.prefix;
-};
-client.on(`message`, msg => {
-    if (msg.author.bot) return;
-    if (!msg.content.startsWith(prefix)) return;
-    var args = msg.content.slice(prefix.length).split(` `);
-    var command = `${args[0]}`;
-    switch (command) {
-        case `vip`:
-            if (msg.author.id !== data.ownerID) return;
-            var cmd = args[1];
-            if (!cmd) return msg.reply(`what command you are looking for ?`);
-            switch(cmd) {
-                case `move`:
-                    var guild = args[2];
-                    if (!guild) return msg.reply(`I can't find this server \\:(`);
-                    if (isNaN(parseInt(guild))) return msg.reply(`I can't find this server \\:(`);
-                    if (guild.length !== msg.guild.id.length) return msg.reply(`I can't find this server \\:(`);
-                    msg.channel.send(`invite me to your new server: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&guild_id=${guild}&permissions=2146958847`);
-                    data.guildID = guild;
-                break;
-                case `prefix`:
-                    var prfx = args[3];
-                    if (!prefix) return msg.reply(`prefix ?`);
-                    data.prefix = prfx.trim();
-                    msg.channel.send(`done , my prefix now is : ${prfx}`);
-                break;
-            }
-        break;
-    };
-    fs.writeFileSync(`./data.json`, JSON.stringify(data, (null, 4)));
-});
-client.on(`guildCreate`, guild => {
-    if (guild.id !== data.guildID) guild.leave();
-    setTimeout(function Sweetie(){
-        client.guilds.forEach(g => {
-            if (g.id !== data.guildID) guild.leave();
-        });
-    }, 5000);
-});
-
-client.on(`ready`, () => {
-    client.guilds.forEach(guild => {
-        if (guild.id !== data.guildID) guild.leave();
-    });
 });
 
 
