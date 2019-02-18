7cca322f463f6d03492313fadfebdbd1775016ce
Alaseri <41761073+imomarksasa@users.noreply.github.com>
2019-02-18 23:15:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,68 +3,7 @@
 
 
 
-client.on('ready',  () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by BadGuY');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
-client.user.setStatus("idle");
-});
 
-
-
-client.on('message', async message => {
-if(message.content.startsWith(prefix + "bcall")) {
-  let i = client.users.size;
-  if(message.author.id !== '321476121956974595') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
-  var args = message.content.split(' ').slice(1).join(' ');
-  if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
-  setTimeout(() => {
-    message.channel.send(`تم الارسال لـ ${i} شخص`)
-  }, client.users.size * 500);
-  client.users.forEach(s => {
-    s.send(args).catch(e => i--);
-  });
-}
-});
-
-
-const devs = ["321476121956974595"];
-const adminprefix = ["-"];
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith('ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === ("leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith('wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith('mils')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith('st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith('setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith('setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
 
 
 if (!Discord) {
